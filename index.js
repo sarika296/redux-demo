e34a00ee9214a96a4e502dbc4d8fdd91dbb8c41b
sarika296 <sarikadewangan94@gmail.com>
2020-06-10 21:50:39 +0530

    redux reducer

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ function buyCake() {
 const initialState = {
   numOfCakes: 10
 }
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case BUY_CAKE: return {
+      ...state,
+      numOfCakes: state.numOfCakes - 1
+    } 
+
+    default: return state
+  }
+}
